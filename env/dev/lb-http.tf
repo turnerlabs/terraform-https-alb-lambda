63e13c86d22d0fce3fb14b01b275176e51e3aa57
John Ritsema <john.ritsema@turner.com>
2019-07-17 09:16:32 -0400

    initial (#2)

---
+++ env/dev/lb-http.tf
@@ -0,0 +1,28 @@
+# adds an http listener to the load balancer and allows ingress
+# (delete this file if you only want https)
+
+resource "aws_alb_listener" "http" {
+  load_balancer_arn = "${aws_alb.main.id}"
+  port              = "${var.lb_port}"
+  protocol          = "${var.lb_protocol}"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_security_group_rule" "ingress_lb_http" {
+  type              = "ingress"
+  description       = "${var.lb_protocol}"
+  from_port         = "${var.lb_port}"
+  to_port           = "${var.lb_port}"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.nsg_lb.id}"
+}
